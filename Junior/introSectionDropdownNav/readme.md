b4a07b29e3c63bff80752c69338dc2fe266ce099
Rachel <106885713+rachview@users.noreply.github.com>
2022-10-16 22:12:27 -0400

    introSectionDropdownNav
    Frontend Mentor: Intro Section Dropdown Navigation

---
+++ Junior/introSectionDropdownNav/readme.md
@@ -0,0 +1,15 @@
+# Frontend Mentor - Intro section with dropdown navigation solution
+
+This is a solution to the [Intro section with dropdown navigation challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/intro-section-with-dropdown-navigation-ryaPetHE5). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+Credit: All images and designs were provided by FrontEnd Mentor.
+
+Task: Follow the design created by FrontEnd Mentor. Keep it as close to the design as possible, and using the styles they have chosen.
+
+What was used: HTML 5, SASS, npm, JS.
+
+In detail: Wrote HTML code, moved onto SASS (first time using it), and worked mobile to desktop. Followed it up with JS for the nav bar.
+
+Problems: First time using Sass, had to learn how to use it--will require lots of practice to get use to it. Need to learn more about compliers in order to get the JS and SASS to talk together. Sass takes some getting use to, right now I feel more comfortable with CSS (Sass seems overboard).
+
+Final Thoughts: Difficulty was easy, but the JS took some work to update (will need to research this more). Followed Kevin Powell's tutorial on running Sass through npm: https://www.youtube.com/watch?v=o4cECvhrBo8. Learned some basic Sass through Gray Simon at DesignCourse: https://www.youtube.com/watch?v=roywYSEPSvc.
---
+++ Junior/introSectionDropdownNav/src/assets/js/index.js
@@ -0,0 +1,50 @@
+const menu = document.querySelectorAll("#menu");
+const nav = document.querySelector("nav");
+const bgfade = document.querySelector(".bg");
+const close = document.querySelector("#close-menu-el");
+const open = document.querySelector("#open-menu-el");
+const featureBtn = document.querySelector("#feature-btn");
+const companyBtn = document.querySelector("#company-btn");
+const featureList = document.querySelector("#feature-list");
+const companyList = document.querySelector("#company-list");
+const featureArrow = document.querySelector("#feature-arrow");
+const companyArrow = document.querySelector("#company-arrow");
+
+menu.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+    bgfade.classList.toggle("fade");
+    if (nav.classList.contains("nav-open")) {
+      open.style.display = "none";
+      close.style.display = "block";
+    } else {
+      open.style.display = "block";
+      close.style.display = "none";
+    }
+  });
+});
+
+featureBtn.addEventListener("mouseover", () => {
+  // featureList.classList.toggle("hidden");
+  // featureArrow.classList.toggle("rotate");
+  openMenu(featureList, featureArrow);
+});
+
+companyBtn.addEventListener("mouseover", () => {
+  // companyList.classList.toggle("hidden");
+  // companyArrow.classList.toggle("rotate");
+  openMenu(companyList, companyArrow);
+});
+
+function openMenu(list, arrow) {
+  list.classList.toggle("hidden");
+  arrow.classList.toggle("rotate");
+  clearInterval(timer(list, arrow));
+}
+
+function timer(list, arrow) {
+  setTimeout(() => {
+    list.classList.toggle("hidden");
+    arrow.classList.toggle("rotate");
+  }, 4000);
+}
